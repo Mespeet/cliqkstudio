3f0d52a88ebf8d8e79081e87a677fe7e34b1f50f
Maarten Speet <70283394+Mespeet@users.noreply.github.com>
2024-09-09 09:47:03 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script defer src="https://views.cliqk.studio/script.js" data-website-id="2ad39aa0-516a-41af-9df2-aa3ec29c7e19"></script>
-      </head>
+      <Script src="https://views.cliqk.studio/script.js" data-website-id="2ad39aa0-516a-41af-9df2-aa3ec29c7e19" />
       <body className={inter.className}>{children}</body>
     </html>
   );
